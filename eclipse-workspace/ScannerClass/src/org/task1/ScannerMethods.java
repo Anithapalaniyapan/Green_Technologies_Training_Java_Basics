81811efa18e8c58223d0cd28429a91a0a54f3393
Anithapalaniyapan <147929058+Anithapalaniyapan@users.noreply.github.com>
2024-09-23 16:10:56 +0530

    Add files via upload

---
+++ eclipse-workspace/ScannerClass/src/org/task1/ScannerMethods.java
@@ -0,0 +1,95 @@
+package org.task1;
+import java.util.Scanner;
+//Scanner Class
+public class ScannerMethods {
+	public static void main(String[]args) {
+		
+		Scanner a=new Scanner(System.in);
+		//Scanner Methods
+
+		//nextLine()
+		String c=a.nextLine();
+		System.out.println(c);
+		
+		//next()
+		String b=a.next();
+		System.out.println(b);
+		
+		//nextInt()
+		int d=a.nextInt();
+		System.out.println(d);
+		
+		//nextByte()
+		byte e=a.nextByte();
+		System.out.println(e);
+		
+		//nextShort()
+		short f=a.nextShort();
+		System.out.println(f);
+		
+		//nextFloat()
+		float g=a.nextFloat();
+		System.out.println(g);
+		
+		//nextLong()
+		long h=a.nextLong();
+		System.out.println(h);
+		
+		//nextDouble()
+		double j=a.nextDouble();
+		System.out.println(j);
+		
+		//nextBoolean()
+		boolean i=a.nextBoolean();
+		System.out.println(i);
+		
+		
+//      OUTPUT
+		//1.nextLine()
+		
+//  I/P: My Name is Anitha
+//  O/P: My Name is Anitha
+		
+		//2.next()
+		
+//	I/P: Anitha
+//  O/P: Anitha
+		
+		//3.nextInt()
+		
+//	I/P: 123456789
+//	O/P: 123456789
+		
+		//4.nextByte()
+		
+//	I/P: 25
+//	O/P: 25
+ 
+		//5.nextShort()
+		
+//	I/P: 32767
+//	O/P: 32767
+
+		//6.nextFloat()
+		
+//	I/P: 23.12345
+//	O/p: 23.12345
+
+		//7.nextLong()
+		
+//	I/P: 12345678911
+//	O/P: 12345678911
+		
+		//8.nextDouble()
+		
+//	I/P: 12.123456789111234
+//	O/P: 12.123456789111234
+
+		//9.nextBoolean()
+		
+//	I/p: true
+//	O/p: true
+
+	}
+
+}
